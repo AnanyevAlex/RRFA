3679cf5d282c7181f21c98968b796c84157d2aa0
Tashik <a1113035@yandex.ru>
2018-12-06 00:25:01 +0500

    обновил эффекты

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,15 +1,22 @@
 var popup = document.querySelector(".main-nav__list");
 var btnOpen = document.querySelector(".main-nav__btn-open");
 var btnClose = document.querySelector(".main-nav__btn-close")
+var delayInSec = 500;
 
 btnOpen.addEventListener('click', function () {
   popup.classList.add("main-nav__list--open");
+  setTimeout(function () {
+    popup.classList.add("main-nav__list--opacity");
+  }, delayInSec);
   btnOpen.classList.add("main-nav__btn-open--none");
   btnClose.classList.remove("main-nav__btn-close--none");
 });
 
 btnClose.addEventListener('click', function () {
-  popup.classList.remove("main-nav__list--open");
+  popup.classList.remove("main-nav__list--opacity");
+  setTimeout(function () {
+    popup.classList.remove("main-nav__list--open");
+  }, delayInSec);
   btnOpen.classList.remove("main-nav__btn-open--none");
   btnClose.classList.add("main-nav__btn-close--none");
 });
